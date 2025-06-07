e657af5555d50eb05ecc61c73fdab2047fb8fd51
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-07 13:24:31 +0530

    Done with let let const and var

---
+++ JS_wtih_harry/video55.js
@@ -0,0 +1,32 @@
+// let var const 
+// js standard echma script
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+var _a = "Shunham"
+// var 12a = "Shunh"
+
+
+
+console.log(a+b+8);
+console.log(typeof a,typeof b,typeof c);
+
+const a1 = 6;
+// a1 = a1 + 1;  const can't be modified
+
+{
+    let a = 66;
+    console.log(a);
+}
+console.log(a) // becoz let is blocks code
+
+{
+    var a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+
+
