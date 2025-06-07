bcb67e62e436403edafe31e7f22b81cdca70838e
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-07 13:09:54 +0530

    Introduce with document

---
+++ JS_wtih_harry/video54.js
@@ -0,0 +1,17 @@
+alert("Hello");
+
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is looking like a wow");
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+if(isTrue){
+    console.log("Computer is not Blasting")
+}
+else{
+    console.log("Your Computer is blasting")
+}
+console.log("Your number is " + a);
+
+document.title = "HEy i am Sachin"
