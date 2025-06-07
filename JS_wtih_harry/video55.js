5c0f0c95f9e48bf86657d499d08f4d6fe122d855
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-07 13:40:25 +0530

    introduce with object

--- JS_wtih_harry/video55.js
+++ JS_wtih_harry/video55.js
@@ -40,8 +40,24 @@ let r = null;
 console.log(x,y,z,p,q,r)
 console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
 
+// why is type of null object
+
 
 //lenter can tell in VS code when u do wrong
+
+
+let o = {
+    name: 'Harry',
+    "job code": 6500,
+    "is handsome": true,
+}
+
+console.log(o);
+o.salary = "100 crore"
+console.log(o);
+o.salary = "500 crore";
+console.log(o)
+    
 
 
 
