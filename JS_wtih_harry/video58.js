2e38973f37898e0eb213d0ccc00e3db4a02fca7a
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-09 09:40:49 +0530

    introduced with function

---
+++ JS_wtih_harry/video58.js
@@ -0,0 +1,16 @@
+function isName(name){
+
+    console.log(`Hey ${name} you are bad!`)
+    console.log(`Hey ${name} you are good!`)
+    console.log(`Hey ${name} your tshirt is nice!`)
+    console.log(`Hey ${name} your course is good too`)
+}
+
+isName("sachin")
+
+function sum(a,b)
+{
+    console.log(`The sum of these numbers is ${a+b}`)
+}
+
+sum(5,4)
