699baed8c3f5b5c005174958a14f6fb33bb8a6f2
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-09 09:51:21 +0530

    done with arrow function

--- JS_wtih_harry/video58.js
+++ JS_wtih_harry/video58.js
@@ -14,3 +14,23 @@ function sum(a,b)
 }
 
 sum(5,4)
+
+
+function sum2(a,b,c=3){
+    console.log(`sum is ${a+b+c}`)
+}
+
+sum2(5,6);
+sum2(3);   //NaN
+sum(5,6,1)
+
+//Arrow function
+let z=10;
+const func1 = (z)=>{
+    console.log("I am an arrow function",z)
+}
+
+func1(55);
+func1(5);
+
+
